309f27417ad727197a16f26a90ee9a91ab35eaf4
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-03-17 10:15:11 +0100

    untracked 'dist' folder form git

--- backend/dist/lib/transactions/ContractFunction.d.ts
+++
@@ -1,9 +0,0 @@
-import { JsonFragment } from '@ethersproject/abi';
-export default class ContractFunction {
-    private requestMsg;
-    functionArguments: any[];
-    private abiItem;
-    constructor(abiItem: JsonFragment, requestMsg: string);
-    encode(): string;
-    decode(): any[];
-}
