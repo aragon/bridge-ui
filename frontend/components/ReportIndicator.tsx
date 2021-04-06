086cf97e7d69817d2055107626025c3257f21e2d
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2021-04-06 14:15:19 +0200

    creating a new card for the indicator

--- frontend/components/ReportIndicator.tsx
+++ frontend/components/ReportIndicator.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Link from "next/link";
-import { Box, GU, Button, EmptyStateCard } from "@aragon/ui";
 import { PROBLEM_ICON, SOLUTION_ICON } from "../lib/constants";
+import IndicatorCard from "./Cards/IndicatorCard";
+import { useRouter } from "next/router";
 
 type IndicatorInfo = {
   projectId: string;
@@ -9,23 +9,31 @@ type IndicatorInfo = {
 };
 
 function ReportIndicator({ projectId, problemHash = null }: IndicatorInfo) {
+  const router = useRouter();
+
   let cardImage: string;
   let cardText: string;
+  let cardSubTitle: string;
   let cardActionText: string;
   let pathName: string;
   let query: any;
+  let buttonMode: string;
   if (problemHash === null) {
     cardImage = PROBLEM_ICON;
     cardText = "Report a problem";
+    cardSubTitle="... and get rewarded";
     cardActionText = "Create new problem";
     pathName = "/[projectId]/proposal";
     query = { projectId: projectId };
+    buttonMode = "negative";
   } else {
     cardImage = SOLUTION_ICON;
     cardText = "Report a solution";
+    cardSubTitle="... and get rewarded";
     cardActionText = "Create new solution";
     pathName = "/[projectId]/[problemId]/proposal";
     query = { projectId: projectId, problemId: problemHash };
+    buttonMode = "strong";
   }
   // RENDERER ============================================================================
 
@@ -33,20 +41,19 @@ function ReportIndicator({ projectId, problemHash = null }: IndicatorInfo) {
     <div
       className="sticky"
     >
-      <EmptyStateCard
-        illustration={<img src={cardImage} alt="" />}
+      <IndicatorCard 
+        ilustration={cardImage}
         text={cardText}
-        action={
-          <Link
-            href={{
-              pathname: pathName,
-              query: query,
-            }}
-            passHref
-          >
-            <Button mode="negative" external={false} label={cardActionText} />
-          </Link>
-        }
+        subText={cardSubTitle}
+        buttonAction={() => {
+          let urlObject = {
+            pathname: pathName,
+            query: query,
+          };
+          router.push(urlObject);
+        }}
+        buttonMode={buttonMode}
+        buttonText={cardActionText}
       />
     </div>
   );
